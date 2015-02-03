c8ec5ee34808b175e46f7c27b06f5f2a77ca078c
Adam Silcott <adsilcott@gmail.com>
2015-02-03 15:11:35 -0500

    Added integration of UTiled plugin to import tilemaps. Created RobotBattery class for battery related things.

---
+++ Assets/Scripts/Robots/RobotBattery.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class RobotBattery
+{
+	public float max = 16f;
+	public float level = 16f;
+	public float standingDrain = 0.0f;
+	public float movingDrain = 0.2f;
+	public float jumpingDrain = 0.4f;
+	public float specialDrain = 0.5f;
+	
+	public bool Drain(float amount)
+	{
+		level -= amount;
+		
+		if (level <= 0)
+			return true;
+		else
+			return false;
+		
+	}
+}
