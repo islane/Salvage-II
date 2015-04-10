ae9eb404ada9a870a84bb7bf71bad155fcf41777
adsilcott <adsilcott@gmail.com>
2015-04-10 16:26:48 -0400

    Test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("What???");
+	
+	}
+}
